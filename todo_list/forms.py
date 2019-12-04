bcba8473fc11006455e5eab2c462cc29e38342b4
Donald Vallejo <donny.vallejo@gmail.com>
2019-12-04 00:04:16 -0800

    moving to models and views

---
+++ todo_list/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.db import models
+from .models import List
+
+class ListForm(forms.ModelForm):
+    class Meta:
+        model = List
+        fields = ["item", "completed"]
--- todo_list/models.py
+++ todo_list/models.py
@@ -4,6 +4,5 @@ class List(models.Model):
      item = models.CharField(max_length=200)
      completed = models.BooleanField(default=False)
 
-
      def __str__(self):
-        return self.item 
+        return self.item + ' | ' + str(self.completed)
