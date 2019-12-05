2fd59982340c26e3cbb8f67cf574f1fe24fda104
Donald Vallejo <donny.vallejo@gmail.com>
2019-12-05 01:48:21 -0800

    finished to do app

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('delete/,list-id.', views.delete, name='delete'),
+    path('delete/,<list_id>', views.delete, name='delete'),
+    path('cross_off/,<list_id>', views.cross_off, name='cross_off'),
+    path('uncross_off/,<list_id>', views.uncross_off, name='uncross_off'),
+    path('edit/,<list_id>', views.edit, name='edit'),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
 from django.contrib import messages
+from django.http import HttpResponseRedirect
+
 
 def home(request):
 
@@ -25,4 +27,32 @@ def about(request):
 def delete(request, list_id):
     item = List.objects.get(pk=list_id)
     item.delete()
-    messages.success(request, ('Item has Been Deleted'))
+    messages.success(request, ('Item Has Been Deleted!'))
+    return redirect('home')
+
+def cross_off(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    return redirect('home')
+
+def uncross_off(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    return redirect('home')
+
+def edit(request, list_id):
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
+
+        form = ListForm(request.POST or None, instance=item)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Item has Been Edited'))
+            return redirect('home')            
+            
+    else:
+        item = List.objects.get(pk=list_id)
+        return render(request, 'edit.html', {'item': item})
