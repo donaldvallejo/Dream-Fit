d75555f024ee9d7ccbfe7bfc0fc7c3168fcd4d71
Donald Vallejo <donny.vallejo@gmail.com>
2019-12-12 19:38:50 -0800

    DONE

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,5 +2,32 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from accounts.models import Testuser
 
-# Create your tests here.
+class userTests(TestCase):
+    def test_True(self):
+        # Create two objects of type List.
+        Testuser.objects.create(name="bryan", email="dude69@pornhub.com", password="69p0rn3ub")
+        Testuser.objects.create(name="donny", email="girl96@pornhub.com", password="pron69696")
+        
+        # Get created objects of type list from db 
+        bryan = Testuser.objects.get(name="bryan")
+        donny = Testuser.objects.get(name="donny")
+
+        # Call test assertions
+        self.assertEqual(bryan.__str__(), "bryan | dude69@pornhub.com | 69p0rn3ub")
+        self.assertEqual(donny.__str__(), "donny | girl96@pornhub.com | pron69696")
+
+    def test_False(self):
+        # Create two objects of type List.
+        Testuser.objects.create(name="guy", email="dudeee465@pornhub.com", password="smfnsd35nf")
+        Testuser.objects.create(name="girl", email="giirlr435@pornhub.com", password="whadupypo45")
+        
+        # Get created objects of type list from db 
+        guy = Testuser.objects.get(name="guy")
+        girl = Testuser.objects.get(name="girl")
+
+        # Call test assertions
+        self.assertNotEqual(guy.__str__(), "guy | dsfgsdf57789@pornhub.com | 69sdgg3ub")
+        self.assertNotEqual(girl.__str__(), "girl | gifgdsf567@pornhub.com | pron457454")
+
--- todo_list/tests.py
+++ todo_list/tests.py
@@ -3,27 +3,28 @@ from django.test import TestCase
 from todo_list.models import List
 
 class ListTests(TestCase):
-    def test_multiple_pages(self):
-        # Make some test data to be displayed on the page.
-        user = User.objects.create()
+    def test_True(self):
+        # Create two objects of type List.
+        List.objects.create(item="bryans_stuff", price=10.00, quantity=2, completed=False)
+        List.objects.create(item="donnys_stuff", price=100.00, quantity=3, completed=False)
+        
+        # Get created objects of type list from db 
+        bryans_stuff = List.objects.get(item="bryans_stuff")
+        donnys_stuff = List.objects.get(item="donnys_stuff")
 
-        List.objects.create(item="My Test Page", content="unittest", author=user)
-        List.objects.create(title="Another Test Page", content="test", author=user)
+        # Call test assertions
+        self.assertEqual(bryans_stuff.__str__(), "bryans_stuff | 10.00 | 2 | False")
+        self.assertEqual(donnys_stuff.__str__(), "donnys_stuff | 100.00 | 3 | False")
 
-        # Issue a GET request to the MakeWiki homepage.
-        # When we make a request, we get a response back.
-        response = self.client.get('/')
+    def test_False(self):
+        # Create two objects of type List.
+        List.objects.create(item="bryan", price=1.00, quantity=1, completed=True)
+        List.objects.create(item="donny", price=1.00, quantity=1, completed=True)
+        
+        # Get created objects of type list from db 
+        bryan = List.objects.get(item="bryan")
+        donny = List.objects.get(item="donny")
 
-        # Check that the response is 200 OK.
-        self.assertEqual(response.status_code, 200)
-
-        # Check that the number of pages passed to the template
-        # matches the number of pages we have in the database.
-        responses = response.context['all_items']
-        self.assertEqual(len(responses), 2)
-
-        self.assertQuerysetEqual(
-            responses,
-            ['<Page: My Test Page>', '<Page: Another Test Page>'],
-            ordered=False
-        )
+        # Call test assertions
+        self.assertNotEqual(bryan.__str__(), "bryan | 1.00 | 2 | True")
+        self.assertNotEqual(donny.__str__(), "donny | 1.00 | 2 | True")
